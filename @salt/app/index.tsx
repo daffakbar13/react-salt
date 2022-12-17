51da4804932715831dec7539a79327bcaf3f4177
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-17 20:13:11 +0700

    update

--- @salt/app/index.tsx
+++ @salt/app/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { Routes } from '../routes';
 import './style.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export default function App() {
   return <Routes/>;
