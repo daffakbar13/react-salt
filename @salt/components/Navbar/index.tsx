904ab6de28fd17aab28e34fbb53a939478189887
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-17 22:01:16 +0700

    update

--- @salt/components/Navbar/index.tsx
+++ @salt/components/Navbar/index.tsx
@@ -1,5 +1,19 @@
 import * as React from "react";
+import Grid from '@mui/material/Grid';
+import { useTheme } from '@mui/material/styles';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+    const theme = useTheme()
+  return (
+    <Grid container spacing={2}>
+        <Grid item xs={4}>
+            <span color={theme.palette.secondary.main}>
+                <h1>Social</h1>
+            </span>
+            <span color="grey">
+                <h1>Network</h1>
+            </span>
+        </Grid>
+    </Grid>
+  );
 }
