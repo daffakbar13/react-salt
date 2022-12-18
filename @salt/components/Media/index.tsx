07b25ef3576548534d903cbc3e6e2397a63cd729
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-19 02:33:45 +0700

    update

--- @salt/components/Media/index.tsx
+++ @salt/components/Media/index.tsx
@@ -1,31 +1,36 @@
-import { Card, CardContent, CardContentProps, CardProps, styled } from '@mui/material'
-import theme from '../../styles/theme'
+import {
+  Card,
+  CardContent,
+  CardContentProps,
+  CardProps,
+  styled,
+} from "@mui/material";
+import theme from "../../styles/theme";
 
-const Collection = styled(Card)<CardProps>(({theme})=>({
-    backgroundColor: theme.palette.secondary.main,
-    borderRadius: 0,
-    boxShadow: 'none'
-}))
+const Collection = styled(Card)<CardProps>(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
+  borderRadius: 0,
+  boxShadow: "none",
+}));
 
-export const MoreAction = styled(Collection)<CardProps>(({theme})=>({
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: theme.palette.secondary.main,
-    boxSizing: 'border-box'
-}))
+export const MoreAction = styled(Collection)<CardProps>(({ theme }) => ({
+  backgroundColor: "transparent",
+  borderWidth: 1,
+  borderStyle: "solid",
+  borderColor: theme.palette.secondary.main,
+  boxSizing: "border-box",
+}));
 
-const Description = styled(CardContent)<CardContentProps>(()=>({
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    left: 0,
-    zIndex: 1,
-    color: theme.palette.primary.main,
-    padding: 10
-}))
+const Description = styled(CardContent)<CardContentProps>(() => ({
+  position: "absolute",
+  bottom: 0,
+  right: 0,
+  left: 0,
+  zIndex: 1,
+  color: theme.palette.primary.main,
+  padding: 10,
+}));
 
-const Media = {Collection, MoreAction, 
-}
+const Media = { Collection, MoreAction, Description };
 
-export default Media
+export default Media;
