aa43e03f3aff53becaded6635e5fa6f6435b5fa7
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-18 09:22:18 +0700

    update

--- @salt/containers/PageHome/sections/sectionVideos.tsx
+++ @salt/containers/PageHome/sections/sectionVideos.tsx
@@ -33,6 +33,18 @@ export default function SectionVideos() {
               </CardActionArea>
             </Card>
           </Grid>
+          <Grid item xs={4}>
+            <Card sx={{ width: "100%" }}>
+              <CardActionArea>
+                <Skeleton variant="rectangular" width="100%" height={118} />
+                <CardContent>
+                  <Skeleton width="100%" height={30} />
+                  <Skeleton width="100%" height={20} />
+                  <Skeleton width="80%" height={20} />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
