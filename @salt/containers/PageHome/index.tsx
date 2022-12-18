51cebe64a7d7dc95ce0acd4a515015bb1423ffe6
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-18 09:05:56 +0700

    update

--- @salt/containers/PageHome/index.tsx
+++ @salt/containers/PageHome/index.tsx
@@ -1,7 +1,24 @@
-import * as React from "react"
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Skeleton,
+  Typography,
+} from "@mui/material";
+import * as React from "react";
 
 export default function PageHome() {
   return (
-    <div>asd</div>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <Skeleton variant="rectangular" width="100%" height={118} />
+        <CardContent>
+          <Skeleton width="100%" height={30} />
+          <Skeleton width="100%" height={20} />
+          <Skeleton width="80%" height={20} />
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
