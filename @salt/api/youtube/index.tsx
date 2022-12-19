e8f507b67c65ed23b29fa72c36863b8038647054
daffakbar13 <63933470+daffakbar13@users.noreply.github.com>
2022-12-19 11:18:26 +0700

    1 file modified

--- @salt/api/youtube/index.tsx
+++ @salt/api/youtube/index.tsx
@@ -1,13 +1,13 @@
-const axios = require("axios");
+import axios from 'axios';
 
 const options = {
   method: 'GET',
   url: 'https://instagram47.p.rapidapi.com/public_user_posts',
-  params: {userid: '1718924098'},
+  params: { userid: '1718924098' },
   headers: {
     'X-RapidAPI-Key': 'd067a0ba46mshf99640e4d2b933dp1432cejsn22b51a4db2dd',
-    'X-RapidAPI-Host': 'instagram47.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'instagram47.p.rapidapi.com',
+  },
 };
 
 export const getYoutubeApi = async (): Promise<any> => {
@@ -17,5 +17,5 @@ export const getYoutubeApi = async (): Promise<any> => {
     .catch((error) => {
       console.log(error);
     });
-    return result
+  return result;
 };
